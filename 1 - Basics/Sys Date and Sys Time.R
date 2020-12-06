8ff69492f7aaec42626e44a6a60adaff532be0e2
Ambareesh Jonnavittula <74958075+ambareesh-j@users.noreply.github.com>
2020-12-06 17:23:39 +0530

    Add files via upload

---
+++ 1 - Basics/Sys Date and Sys Time.R
@@ -0,0 +1,6 @@
+# Write a R program to create the system's idea 
+# of the current date with and without time.
+
+print("System's idea of the current date with and without time:")
+print(Sys.Date())
+print(Sys.time())
